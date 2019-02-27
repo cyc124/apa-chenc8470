125a3bd2966addf5955ad12d9b3ce0ca56213ef6
Cynthia Chen <cynthia58057@gmail.com>
2019-02-27 23:10:09 +0000

    first

---
+++ UG1/SmileyFace.java
@@ -0,0 +1,43 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Canvas;
+
+public class SmileyFace extends Canvas
+{
+   public SmileyFace()    //constructor - sets up the class
+   {
+      setSize(800,600);
+      setBackground(Color.WHITE);
+      setVisible(true);
+   }
+
+   public void paint( Graphics window )
+   {
+      smileyFace(window);
+   }
+
+   public void smileyFace( Graphics window )
+   {
+      window.setColor(Color.BLUE);
+      window.drawString("SMILEY FACE LAB ", 35, 35);
+
+      window.setColor(Color.YELLOW);
+      window.fillOval( 210, 100, 400, 400 );
+
+	window.setColor(Color.BLACK);
+	window.fillOval(300,300,50,50);
+	window.fillOval(500,300,50,50);
+
+	window.fillArc(400,400,50,10,180,360);	//add more code here
+
+
+   }
+}
