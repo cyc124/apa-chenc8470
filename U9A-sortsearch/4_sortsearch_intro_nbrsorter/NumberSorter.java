680934119bcbb967239742df653a737428896bce
Cynthia Chen <cynthia58057@gmail.com>
2019-02-28 21:59:30 +0000

    first

--- U9A-sortsearch/4_sortsearch_intro_nbrsorter/NumberSorter.java
+++ U9A-sortsearch/4_sortsearch_intro_nbrsorter/NumberSorter.java
@@ -14,13 +14,29 @@ public class NumberSorter
 
 	private static int getNumDigits(int number)
 	{
-		int count = 0;
+		int count=0;
+		while (number>0)
+		{
+			number=number/10;
+			count++;
+		}
 		return count;
 	}
 
 	public static int[] getSortedDigitArray(int number)
 	{
-		int[] sorted = null;
+		int[] sorted = new int[getNumDigits(number)];
+		int digit=0;
+		int count=0;
+		while (number>0)
+		{
+			digit=number%10;
+			sorted[count]=digit;
+			number=number/10;
+			count++;
+		}
+		Arrays.sort(sorted);
+
 		return sorted;
 	}
 }
